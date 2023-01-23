ab4eedb30ed7e67cc9651e0f86d99afccb3c3d9e
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2023-01-23 23:08:03 +0530

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,5 +1,5 @@
 /** @jsx h */
-import { serve } from "https://deno.land/std@0.173.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
 import { h, html } from "https://deno.land/x/htm@0.0.10/mod.tsx";
 import { UnoCSS } from "https://deno.land/x/htm@0.0.10/plugins.ts";
 import { format } from "https://deno.land/std@0.173.0/fmt/bytes.ts"
@@ -27,11 +27,11 @@ serve(async (req: Request) => {
     title: "Home",
     body: (
       <div class="w-full font-mono">
-        <h2><a href=`https://github.com/${owner}`>{owner}</a>/<a href=`https://github.com/${owner}/${repo}`>{repo}</a></h2>
+        <h2><a href={`https://github.com/${owner}`}>{owner}</a>/<a href={`https://github.com/${owner}/${repo}`}>{repo}</a></h2>
         <hr>
         <pre>{
           for (const file in t) {
-            <a href=`https://github.com/`>{file}</a> ({format(t[file])}){"\n"}
+            <a href={`https://github.com/`}>{file}</a> ({format(t[file])}){"\n"}
           }
         }</pre>
       </div>
